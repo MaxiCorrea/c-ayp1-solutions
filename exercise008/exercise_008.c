1b9ae9a61d01d0000cdb23d2433ad8ee77b27604
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-07-27 13:20:13 -0300

    Es multiplo de 2

---
+++ exercise008/exercise_008.c
@@ -0,0 +1,6 @@
+#include "exercise_008.h"
+
+bool is_multiple_of_2(int number)
+{
+	return number % 2 == 0 ? true : false;
+}
---
+++ exercise008/exercise_008.h
@@ -0,0 +1,3 @@
+#include <stdbool.h>
+
+bool is_multiple_of_2(int number);

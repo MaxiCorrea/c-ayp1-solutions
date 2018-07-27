e0e9cbd43b45b9a349d73bec2fa885da2d2d5ec9
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-07-27 14:13:50 -0300

    Determinar si uno es divisible por 3

---
+++ exercise010/exercise_010.h
@@ -0,0 +1,3 @@
+#include <stdbool.h>
+
+bool check_any_is_divisible_by_3(int number1 , int number2);
---
+++ exercise010/exercise_010_test.c
@@ -0,0 +1,28 @@
+#include "exercise_010.h"
+#include <assert.h>
+
+static void test_without_divisible_by_3() 
+{
+	assert(check_any_is_divisible_by_3(4,5) == false && "check_any_is_divisible_by_3");
+	assert(check_any_is_divisible_by_3(7,2) == false && "check_any_is_divisible_by_3");
+}
+
+static void test_with_one_divisible_by_3() 
+{
+	assert(check_any_is_divisible_by_3(3,2) == true && "check_any_is_divisible_by_3");
+	assert(check_any_is_divisible_by_3(7,12) == true && "check_any_is_divisible_by_3");
+}
+
+static void test_all_divisible_by_3() 
+{
+	assert(check_any_is_divisible_by_3(3,6) == true && "check_any_is_divisible_by_3");
+	assert(check_any_is_divisible_by_3(6,9) == true && "check_any_is_divisible_by_3");
+}
+
+int main(int argc, char const *argv[])
+{
+	test_without_divisible_by_3();
+	test_with_one_divisible_by_3();
+	test_all_divisible_by_3();
+	return 0;
+}

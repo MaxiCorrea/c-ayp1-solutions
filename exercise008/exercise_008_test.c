4c07866e678358a81562264ec88c0c9fea7b79fb
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-07-27 13:29:29 -0300

    Refactorizacion de pruebas

--- exercise008/exercise_008_test.c
+++ exercise008/exercise_008_test.c
@@ -1,20 +1,25 @@
 #include "exercise_008.h"
 #include <assert.h>
 
-static void test_is_multiple_of_2() 
+static void test_with_multiples_of_2() 
 {
-	assert(is_multiple_of_2(2) == true && "is_multiple_of_5");
-	assert(is_multiple_of_2(4) == true && "is_multiple_of_5");
-	assert(is_multiple_of_2(6) == true && "is_multiple_of_5");
-    assert(is_multiple_of_2(8) == true && "is_multiple_of_5");
-	assert(is_multiple_of_2(3) == false && "is_multiple_of_5");
-	assert(is_multiple_of_2(5) == false && "is_multiple_of_5");
-	assert(is_multiple_of_2(7) == false && "is_multiple_of_5");
-    assert(is_multiple_of_2(9) == false && "is_multiple_of_5");		
+	assert(is_multiple_of_2(2) == true && "is_multiple_of_2");
+	assert(is_multiple_of_2(4) == true && "is_multiple_of_2");
+	assert(is_multiple_of_2(6) == true && "is_multiple_of_2");
+	assert(is_multiple_of_2(8) == true && "is_multiple_of_2");	
+}
+
+static void test_without_multiples_of_2()
+{
+	assert(is_multiple_of_2(3) == false && "is_multiple_of_2");
+	assert(is_multiple_of_2(5) == false && "is_multiple_of_2");
+	assert(is_multiple_of_2(7) == false && "is_multiple_of_2");
+	assert(is_multiple_of_2(9) == false && "is_multiple_of_2");	
 }
 
 int main(int argc, char const *argv[])
 {
-	test_is_multiple_of_2();
+	test_with_multiples_of_2();
+	test_without_multiples_of_2();
 	return 0;
 }
